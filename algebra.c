28a4b4249d26f794fd824aacd579aeb9460a292d
Brandon Willett <bcwillett1@gmail.com>
2016-11-17 20:25:58 -0500

    made algebra

---
+++ algebra.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "attributes.h"
+
+int project(C_S_G_Row* table, char** array_of_attributes) {
+
+	while (array_of_attributes != NULL) {
+
+		char* single_attribute = *array_of_attributes;
+
+		if (strcmp(single_attribute, ""))
+
+		array_of_attributes++;
+	}
+
+}
